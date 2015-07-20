879299fb1528eadff0a8f8b690b5ae61d91e45bc
Yiyuan Zhang <markzhang90@gmail.com>
2015-07-20 16:06:01 -0400

    stack sort

---
+++ sort_stack/sort_stack.py
@@ -0,0 +1,18 @@
+# sort stack
+
+def sort_stack(stack):
+    stack_temp = []
+    while any(stack):
+        if len(stack_temp) == 0:
+            stack_temp.append(stack.pop())
+        else:
+            temp = stack.pop()
+            while temp < stack_temp[-1] and len(stack_temp) > 0:
+                stack.append(stack_temp.pop())
+                print stack_temp
+            stack_temp.append(temp)
+    return stack_temp
+
+
+stack = [2,3,2,4,6,5,1]
+print(sort_stack(stack))
